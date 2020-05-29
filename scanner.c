c59835527d8f86cfe5f2ac4a54533b42d0606fec
Chuck Jungmann <chuckj60@gmail.com>
2020-05-29 16:23:13 -0500

    Fixed conditional memory allocation.  Improved memory error checking.

--- scanner.c
+++ scanner.c
@@ -64,11 +64,23 @@ text_link_deleter_t text_link_deleter = NULL;
 
 typedef struct _log_link
 {
-   char *msg;
+   SLDate sldate;
+   char   *host;
+   char   *tag;
+   char   *msg;
+
    struct _log_link *next;
 } LLink;
 
 typedef void(*log_link_deleter_t)(LLink *link);
+
+void free_log_link(LLink *link)
+{
+   free(link->host);
+   free(link->tag);
+   free(link->msg);
+   free(link);
+}
 
 void delete_log_chain_ordered(LLink *link)
 {
@@ -76,8 +88,7 @@ void delete_log_chain_ordered(LLink *link)
    while (link)
    {
       next = link->next;
-      free(link->msg);
-      free(link);
+      free_log_link(link);
 
       link = next;
    }
@@ -90,8 +101,7 @@ void delete_log_chain_recursive(LLink *link)
       if (link->next)
          delete_log_chain_recursive(link->next);
 
-      free(link->msg);
-      free(link);
+      free_log_link(link);
    }
 }
 
@@ -99,37 +109,31 @@ void llink_nonfreer(LLink* link) { ; }
 
 log_link_deleter_t log_link_deleter = NULL;
 
-
-
-typedef void* (*memmaker_t)(size_t size);
 typedef void (*memfreer_t)(void*);
-
-void* stackalloc(size_t size) { return alloca(size); }
-
-memmaker_t memmaker = NULL;
 memfreer_t memfreer = NULL;
-
-
-
-
+void noopfree(void* p) { ; }
 
 // Prepare clargs target variables:
 const char*  Syslog="/var/log/syslog";
 int          ShowHelp = 0;
 unsigned int BuffSize = 16384;
+unsigned int LineBuffSize = 512;
 int          PlainText = 0;       // File processor flag
 int          StackSave = 0;       // Line save method
 int          RecursiveDelete = 0; // 
 int          MemoryRepeats = 1;
 const char*  FieldNames = NULL;
 
+#define getmem(C) ( StackSave ? alloca((C)) : malloc((C)) )
+
 DefLine defs[] = {
-   { 'h', "Show help",            &ShowHelp,      NULL },
-   { 'b', "Change buffer size.",  &BuffSize,      clargs_set_int },
-   { 'p', "Set file to process.", &Syslog,        clargs_set_string },
-   { 't', "File is plain text.",  &PlainText,     NULL },
-   { 'r', "Set repeat count.",    &MemoryRepeats, clargs_set_int },
-   { 's', "Save lines in stack.", &StackSave,     NULL },
+   { 'h', "Show help",                          &ShowHelp,      NULL },
+   { 'b', "Change line-reader buffer size.",    &BuffSize,      clargs_set_int },
+   { 'l', "Change duplicate line buffer size.", &LineBuffSize,  clargs_set_int }, 
+   { 'p', "Set file to process.",               &Syslog,        clargs_set_string },
+   { 't', "File is plain text.",                &PlainText,     NULL },
+   { 'r', "Set repeat count.",                  &MemoryRepeats, clargs_set_int },
+   { 's', "Save lines in stack.",               &StackSave,     NULL },
    /* { 'f', "Comma-separated field names", &FieldNames, clargs_set_string }, */
    CLARGS_END_DEF
 };
@@ -150,12 +154,12 @@ void text_file_processor(LRScope *scope)
    while (lr_get_line(scope, &line, &line_end))
    {
       linelen = line_end - line;
-      newline = (char*)(*memmaker)(1 + linelen);
+      newline = (char*)getmem( 1 + linelen );
       if (newline)
       {
          memcpy(newline, line, linelen);
 
-         new = (TLink*)(*memmaker)(sizeof(TLink));
+         new = (TLink*)getmem(sizeof(TLink));
          if (new)
          {
             memset(new, 0, sizeof(TLink));
@@ -189,16 +193,97 @@ void text_file_processor(LRScope *scope)
 void log_file_processor(LRScope *scope)
 {
    // for now...
-   /* LLink *new, *root = NULL; */
-   LLink *root = NULL;
+   LLink *new, *tail = NULL, *root = NULL;
 
    const char *line, *line_end;
+   char *dupline = (char*)getmem(LineBuffSize);
+   
+   int linelen;
+
+   SLDate sldate;
+   char *host, *tag, *msg;
+
    while (lr_get_line(scope, &line, &line_end))
    {
+      new = NULL;
+
+      // Make non-const copy of line in which
+      // characters can be replaced with '\0'.
+      linelen = line_end - line;
+
+      if (linelen > LineBuffSize)
+         goto line_too_long;
+
+      memcpy(dupline, line, linelen);
+      dupline[linelen] = '\0';
+
+      if (syslog_break_line(dupline, &sldate, &host, &tag, &msg))
+      {
+         new = (LLink*)getmem(sizeof(LLink));
+         if (new)
+         {
+            memset(new, 0, sizeof(LLink));
+            memcpy(&new->sldate, &sldate, sizeof(SLDate));
+
+            linelen = strlen(host) + 1;
+            new->host = (char*)getmem(linelen);
+            if (new->host)
+            {
+               memcpy(new->host, host, linelen);
+
+               linelen = strlen(tag) + 1;
+               new->tag = (char*)getmem(linelen);
+               if (new->tag)
+               {
+                  memcpy(new->tag, tag, linelen);
+
+                  linelen = strlen(msg) + 1;
+                  new->msg = (char*)getmem(linelen);
+                  if (new->msg)
+                  {
+                     memcpy(new->msg, msg, linelen);
+
+                     if (tail)
+                     {
+                        tail->next = new;
+                        tail = new;
+                     }
+                     else
+                        root = tail = new;
+                  }
+                  else
+                     goto memfailure;  // for new->msg
+               }
+               else
+                  goto memfailure;     // for mew->tag
+            }
+            else
+               goto memfailure;        // for new->host
+         }
+         else
+            goto memfailure;           // for new (LLink*)
+      }
    }
 
+   goto cleanup;
+
+  line_too_long:
+   fprintf(stderr, "linelen %d is too long for line buffer %d.\n", linelen, LineBuffSize);
+   goto cleanup;
+
+  memfailure:
+   fprintf(stderr, "Out of memory, abandoning the loop.\n");
+   if (new)
+      (*log_link_deleter)(new);
+
+  cleanup:
+
    (*log_link_deleter)(root);
+
+   // Delete worker 
+   (*memfreer)(dupline);
 }
+
 
 
 // Picks the appropriate processor according to user selections:
@@ -206,10 +291,9 @@ void set_processors(void)
 {
    // Set memory allocator
    if (StackSave)
-      memmaker = stackalloc;
+      memfreer = noopfree;
    else
-      memmaker = malloc;
-   /* memmaker = StackSave ? __builtin_alloca : malloc; */
+      memfreer = free;
 
    // Set memory freer (or not)
    if (PlainText)
